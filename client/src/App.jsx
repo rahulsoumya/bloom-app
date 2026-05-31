9d462f9f64ae7578577b0275c6232b1694a0e6e3
Rahul <sethysoumyaranjan575@gmail.com>
2026-05-31 10:21:19 +0530

    fix footer import error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+//import Footer from './components/Footer'
 import ProtectedRoute from './components/ProtectedRoute'
 
 import Login from './pages/Login'
@@ -178,7 +178,7 @@ const App = () => {
 
           </main>
 
-          <Footer />
+          {/* <Footer /> */}
 
         </div>
 
